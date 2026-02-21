7e15ef26cd5476fc4d90eb5cead980420e2a472e
Lucas Schweickert <cyberfly@gmx.net>
2026-02-21 14:42:13 +0100

    added text to node parsing

--- src/parser.py
+++ src/parser.py
@@ -72,3 +72,13 @@ def split_nodes_link(old_nodes):
         if after:
             new_nodes.append(TextNode(after, TextType.TEXT))
     return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_image(nodes)
+    return nodes
+
--- src/test_parser.py
+++ src/test_parser.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from parser import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from parser import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
 
 class TestConverter(unittest.TestCase):
     def test_bold(self):
@@ -125,8 +125,33 @@ class TestConverter(unittest.TestCase):
             new_nodes
         )
  
-
-
+    def test_text_to_textnodes1(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes
+        )
+ 
+    def test_text_to_textnodes2(self):
+        text = "This is text with no special features."
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [TextNode("This is text with no special features.", TextType.TEXT)],
+            nodes
+        )
+  
 
        
 if __name__ == "__main__":
